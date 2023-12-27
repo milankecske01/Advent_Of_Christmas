6fd13277cb26b31cf59183cbc270508d5fa349b2
milankecske <karvalics.milan@gmail.com>
2023-12-27 19:07:44 +0100

    day8 part1 done

---
+++ Advent of code day8 task1/Program.cs
@@ -0,0 +1,39 @@
+﻿#region beolvasás
+
+StreamReader f = new StreamReader("input.txt");
+string instructions=f.ReadLine();
+f.ReadLine();
+Dictionary<string, string[]> maps = new Dictionary<string, string[]>();
+string current = "AAA";
+
+while (!f.EndOfStream)
+{
+    string line= f.ReadLine();
+    maps[line.Substring(0, 3)] = new string[] {line.Substring(7,3), line.Substring(12, 3) };
+}f.Close();
+#endregion
+
+#region feldolgozás
+
+int i = 0;
+uint output = 0;
+while (current != "ZZZ")
+{
+    if (instructions[i] == 'L')
+    {
+        current = maps[current][0];
+    }
+    else
+    {
+        current = maps[current][1];
+    }
+    i++;
+    if (i==instructions.Length)
+    {
+        i = 0;
+    }
+    Console.WriteLine(current);
+    output++;
+}
+Console.WriteLine(output);
+#endregion feldolgozás
